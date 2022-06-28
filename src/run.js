32b661d72060d9a5091bb091d542d8cc329cc9ee
william <287353354@qq.com>
2022-06-28 16:50:08 +0800

    surpport optional source branch

--- src/run.js
+++ src/run.js
@@ -7,11 +7,10 @@ async function run() {
   try {
     const token = core.getInput('github_token');
     const params = github.context.repo; // owner and repo
-    params.head = github.context.sha;
-    params.base = core.getInput('destination_branch'); // default is staging, see action.yml
+    params.head = core.getInput('source_branch'); // default is master, see action.yml
+    params.base = github.context.sha; // cuurent commit sha
 
     const octokit = github.getOctokit(token);
-
     // https://docs.github.com/en/rest/reference/commits#compare-two-commits
     const response = await octokit.rest.repos.compareCommits(params);
 
@@ -21,7 +20,7 @@ async function run() {
     }
 
     if(response.data.merge_base_commit.sha != params.head) {
-      throw `${params.head} is not merged to ${params.base}`
+        throw `${params.head} is not merged to ${params.base} and response is ${response.data.merge_base_commit.sha}`
     }
 
     core.notice(`${params.head} is merged to ${params.base}`, { title: "Merged" });
